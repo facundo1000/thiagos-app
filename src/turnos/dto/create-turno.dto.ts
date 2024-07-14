393437003b0dd00abe22475d679e678adf14802a
facundo1000 <martinez.facundo85@gmail.com>
2024-07-14 18:48:16 -0300

    add turnoDto

--- src/turnos/dto/create-turno.dto.ts
+++ src/turnos/dto/create-turno.dto.ts
@@ -1,1 +1,11 @@
-export class CreateTurnoDto {}
+/* eslint-disable prettier/prettier */
+import { Cliente, Usuario } from "@prisma/client";
+
+/* eslint-disable prettier/prettier */
+export class CreateTurnoDto {
+  fecha: Date;
+  hora: Date;
+  usuario: Usuario;
+  cliente: Cliente;
+  activo: boolean;
+}
