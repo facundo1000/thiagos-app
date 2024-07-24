a4826b3087b6d9de0e52148fac913a2dfaee1fca
facundo1000 <martinez.facundo85@gmail.com>
2024-07-24 11:13:54 -0300

    typo

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,6 @@ async function bootstrap() {
   app.setViewEngine("hbs");
 
   hbs.registerPartials(join(__dirname, "..", "views", "partials"));
-  // hbs.registerHelper("formatDate", function (date: Date, format) {});
 
   await app.listen(3000);
 }
