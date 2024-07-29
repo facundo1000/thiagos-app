ee09473109fcded8d8444367cd4bc544a14eebd5
facundo1000 <martinez.facundo85@gmail.com>
2024-07-29 20:37:05 -0300

    typo

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { NestExpressApplication } from "@nestjs/platform-express";
 import * as hbs from "hbs";
 import { join } from "path";
 import { AppModule } from "./app.module";
-import { Logger } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
